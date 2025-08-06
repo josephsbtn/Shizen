670f0120fa4714883ba43d1d0fc4cc45d50993e8
josephsbtn <josephsebastian8107@gmail.com>
2025-08-06 12:28:21 +0700

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,12 +5,12 @@ const { dbconnect } = require("./config/database.js");
 
 const predictRoutes = require("./routes/predict.js");
 const userRoutes = require("./routes/user.js");
-const { getDiease } = require("./service/predictService.js");
-
+const challangeRoutes = require("./routes/challanges.js");
 app.use(express.json());
 app.use(cors());
 dbconnect()
   .then(() => {
+    app.use("/challange", challangeRoutes);
     app.use("/predict", predictRoutes);
     app.use("/users", userRoutes);
     app.listen(8000, () => {
