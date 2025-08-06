b038a55158c2db1062d7bf3b78c8c90ef0eb8b75
josephsbtn <josephsebastian8107@gmail.com>
2025-08-06 18:52:35 +0700

    Update config.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,4 +1,4 @@
-const KEY_OPENWEATHER = "e7fef929605b6a73c6921e4fec83f6ce";
+const KEY_OPENWEATHER = "345c0369e5fcb7ce9743497e6aeac1b9";
 const KEY_AQI = "9883ecf1e24192cf9e66f368d1ec6350c55e2be1 ";
 const DB_URI = "mongodb://127.0.0.1:27017/FIT";
 
